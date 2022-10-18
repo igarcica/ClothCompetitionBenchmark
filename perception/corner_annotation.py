c0b572c11faa02271310e9f810e136e94db138a2
Irene Garcia-Camacho <igarcia@iri.upc.edu>
2022-10-18 16:03:36 +0200

    Joined codes into perception.py 	- Inputs: 		- path to image with aruco layout (-i) 		- path to result image with corners of cloth (-ii) 		- team name (used as path of output folder) 		- trial number (for saving results) 	- Calls px_to_cm.py 	- Calls corner_annotation.py to define groundtruth 	- (TODO) Call scoring code. Compare between csv 	- (TODO) Take into account several corners (compare them by proximity?) 	- (TODO) Save results
    Defined functions for corner_annotation.py

--- perception/corner_annotation.py
+++ perception/corner_annotation.py
@@ -10,27 +10,10 @@ import cv2
 import math
 import csv
 
-# read a colourful image
-#img_path = 'rulebook/towel.jpg'
-img_path = 'cloth.jpg'
-scale_percent = 40 # percent of original size
-
-#Image to select corners
-img_1 = cv2.imread(img_path)
-# resize image
-width = int(img_1.shape[1] * scale_percent / 100)
-height = int(img_1.shape[0] * scale_percent / 100)
-dim = (width, height)
-img_1 = cv2.resize(img_1, dim, interpolation = cv2.INTER_AREA)
-cv2.imshow('Cloth', img_1) 
-
-#Set internal variables
-corner = False
-corner_x = 0
-corner_y = 0
 
 def Mouse_Event(event, x, y, flags, param):
-    global corner, corner_x, corner_y
+    global corner, corner_x, corner_y, vect_end_x, vect_end_y
+    img_1 = param
     if event == cv2.EVENT_LBUTTONDOWN:
         print("x: ", x)
         print("y: ", y)
@@ -51,8 +34,10 @@ def Mouse_Event(event, x, y, flags, param):
             print("modulo: ", modulo)
             corner = False # To draw only one vector
             # Save corner point + origin vector point
-            data=[corner_x, corner_y, x, y]
-            writer.writerow(data)
+            vect_end_x = x
+            vect_end_y = y
+#            data=[corner_x, corner_y, x, y]
+#            writer.writerow(data)
 
 #def Vect_Event(event, x, y, flags, param):
 #    global corner_x, corner_y
@@ -71,19 +56,60 @@ def Mouse_Event(event, x, y, flags, param):
 #            filei.close()
 #
 
-# set Mouse Callback method
-cv2.setMouseCallback('Cloth', Mouse_Event)
-#cv2.setMouseCallback('Corner', Vect_Event)
+def define_groundtruth(img_path): 
+    global corner, corner_x, corner_y, vect_end_x, vect_end_y
 
-filei = open('test.csv','w')
-writer=csv.writer(filei)
+    # read a colourful image
+    #img_path = 'rulebook/towel.jpg'
+#    img_path = 'cloth.jpg'
+    scale_percent = 40 # percent of original size
+    
+    #Image to select corners
+    img_1 = cv2.imread(img_path)
+    # resize image
+    width = int(img_1.shape[1] * scale_percent / 100)
+    height = int(img_1.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    img_1 = cv2.resize(img_1, dim, interpolation = cv2.INTER_AREA)
+    cv2.imshow('Cloth', img_1) 
+    
+    #Set internal variables
+    corner = False
+    corner_x = 0
+    corner_y = 0
+    vect_end_x = 0
+    vect_end_y = 0
 
-cv2.waitKey(0)
-# Save points in csv
-filei.close()
-# Create folder for team (input team name)
-cv2.imwrite('GT.jpg', img_1) # Save with trial number
-cv2.destroyAllWindows()
+    param = img_1
+    # set Mouse Callback method
+    cv2.setMouseCallback('Cloth', Mouse_Event, param)
+    #cv2.setMouseCallback('Corner', Vect_Event)
+    
+#    filei = open('test.csv','w')
+#    writer=csv.writer(filei)
+    
+    cv2.waitKey(0)
+    # Save points in csv
+#    filei.close()
+#    # Create folder for team (input team name)
+#    cv2.imwrite('GT.jpg', img_1) # Save with trial number
+    cv2.destroyAllWindows()
+
+    groundtruth_img = img_1
+    corner_coord = (corner_x, corner_y) 
+    vect_end_coord = (vect_end_x, vect_end_y)
+#Vector of corners, vector of vectors!
+    return groundtruth_img, corner_coord, vect_end_coord
+
+
+## Test code
+#img_path = 'test/IMG_20221007_174207.jpg'
+#gt_img, corner, vector_end = define_groundtruth(img_path)
+#print("Corner coordinates: ", corner)
+#print("Vector end coordinates: ", vector_end)
+#cv2.imshow("Ground truth Image", gt_img)
+#cv2.waitKey(0)
+
 
 ## REFS
 # https://analyticsindiamag.com/real-time-gui-interactions-with-opencv-in-python/
--- perception/perception.py
+++ perception/perception.py
@@ -1,1 +1,53 @@
-# Input:
+
+import argparse
+import os
+import sys
+sys.path.insert(1, './px_cm/')
+import px_to_cm
+import corner_annotation as corner_an
+
+# Get image with Aruco layout
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--ar_input", required=True, help="path to input image containing ArUCo layout")
+ap.add_argument("-ii", "--input", required=True, help="path to input image containing result of the trial (cloth with perceived corners and vectors)")
+ap.add_argument("-o", "--team", required=True, type=str, default="Team", help="Team name")
+ap.add_argument("-tt", "--trial", required=True, type=int, default=1, help="Trial numbber")
+ap.add_argument("-obj", "--object", type=str, default="med_towel", help="Object from the Household Cloth Object Set")
+# number of correct corners
+args = vars(ap.parse_args())
+
+
+if not os.path.exists(args["team"]):
+    os.mkdir(args["team"])
+output_path = args["team"]
+
+trial = args["trial"]
+
+
+# Get px/cm ratio
+print("\033[94mGetting pixel/centimeter ratio... \033[0m")
+px_cm_ratio, px_cm_area_ratio = px_to_cm.transform_perspective(args["ar_input"])
+print("px to cm ratio: ", px_cm_ratio)
+print("px to cm AREA ratio: ", px_cm_area_ratio)
+
+# Define ground truth corners and grasping vectors
+groundtruth_img, corner_coord, vect_end_coord = corner_an.define_groundtruth(args["input"])
+print("Corner coordinates: ", corner_coord)
+print("Grasping vector end coordinates: ", vect_end_coord)
+
+# Compare corners - Error
+# Compare vectors (If corner correct) - Error
+
+
+
+## Save results
+## Groundtruth image
+#cv2.imwrite(output_path + "trial" + str(trial) + "_cont.jpg", contour_img) # Save with trial number
+## Measured perimeter and area
+#filei =open(output_path + "trial" + str(trial) + '.csv','w')
+#writer=csv.writer(filei)
+#row = [px_cm_ratio, px_cm_area_ratio, corner_coord, vect_end_coord, corner_error, vect_error, scoring]
+#writer.writerow(row)
+## Info trial (team, trial, config, object, ...), Ratios, perimeter + area (in px and cm), error, points
+
+#filei.close()
